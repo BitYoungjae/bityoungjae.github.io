b08d0b6adc17fbf0ae5414265d1cda8c1a077dcf
JOOYOUNGJAE <bityoungjae@gmail.com>
2019-12-05 21:31:05 +0900

    Site updated: 2019-12-05 21:31:05

--- js/main.js
+++ js/main.js
@@ -94,3 +94,241 @@
         $('.navbar-main .catalogue').on('click', toggleToc);
     }
 })(jQuery);
+
+{
+    const L = {};
+    const curry = f => (h, ...args) => args.length ? f(h, ...args) : (...args) => f(h, ...args);
+
+    L.map = curry(function* (f, iter) {
+        let previous = null;
+        for (const one of iter) {
+            yield f(one, previous);
+            previous = one;
+        }
+    });
+
+    L.filter = curry(function* (f, iter) {
+        for (const one of iter) {
+            if (f(one)) yield one;
+        }
+    });
+
+    L.take = curry(function* (n, iter) {
+        const iterator = iter[Symbol.iterator]();
+        let now;
+
+        do {
+            now = iterator.next();
+            now.value && (yield now.value);
+
+            if (!--n) break;
+        } while (!now.done);
+    });
+
+    const take = curry((n, iter) => {
+        const result = [];
+        const iterator = iter[Symbol.iterator]();
+        let now = null;
+
+        do {
+            now = iterator.next();
+            now.value && result.push(now.value);
+
+            if (!--n) break;
+        } while (!now.done);
+
+        return result;
+    });
+
+    const reduce = curry(
+        (f, iter, start) => {
+            const iterator = iter[Symbol.iterator]();
+            let acc = null;
+
+            if (start) {
+                acc = start;
+            } else {
+                acc = iterator.next().value;
+            }
+
+            for (const one of iterator) {
+                acc = f(acc, one);
+            }
+
+            return acc;
+        }
+    );
+
+    const go = (...routine) => reduce((acc, now) => now(acc), routine);
+    const pipe = (first, ...routine) => (...args) => go(first(...args), ...routine);
+
+    const testFunc = (f, n = 1000, ...args) => {
+        console.time(f.name);
+        while (n--) {
+            f(...args);
+        }
+        console.timeEnd(f.name);
+    };
+
+    const getElementsList = curry((f, root) => go(
+        Array.from(root.children),
+        L.filter(f),
+        L.map(({
+            tagName,
+            id,
+            textContent
+        }) => {
+            return {
+                tagName,
+                id,
+                textContent,
+                subHeadings: []
+            };
+        })
+    ));
+
+    const isHeading = element => element.nodeName.startsWith("H");
+    const getHeadingElementsList = getElementsList(isHeading);
+
+    const getNumberByHeadingTagName = (tagName) => tagName[tagName.length - 1] >> 0;
+    const getDepthDiff = (one, two) => getNumberByHeadingTagName(one) - getNumberByHeadingTagName(two);
+    const normalizeHeadingTagName = (tagName, depth) => "H" + (getNumberByHeadingTagName(tagName) + depth);
+
+    const normalizeHeadingElementList = (list) => L.map(
+        (element, previous) => {
+            if (!previous) return element;
+            if (element.tagName === "H1") element.tagName = "H2";
+
+            const depthDiff = getDepthDiff(previous.tagName, element.tagName);
+
+            if (depthDiff <= -2) {
+                const tagName = element.tagName;
+                element.tagName = normalizeHeadingTagName(tagName, depthDiff + 1);
+            }
+
+            return element;
+        }, list);
+
+    const getHeadingElementsChain = (list) => {
+        const iterator = list[Symbol.iterator]();
+        const parentPointer = new Map();
+
+        let result = {
+            tagName: "H1",
+            textContent: "",
+            subHeadings: []
+        };
+        let now = iterator.next();
+        let previous = null;
+
+        if (now.value.tagName === "H1") {
+            result = now.value;
+            previous = now.value;
+        } else {
+            result = {
+                tagName: "H1",
+                textContent: now.value.textContent,
+                subHeadings: []
+            };
+
+            result.subHeadings.push(now.value);
+            previous = result;
+        }
+
+        while ((now = iterator.next()) && now.value) {
+            const {
+                tagName
+            } = now.value;
+
+            if (previous.tagName < tagName) {
+                const subArray = [];
+                subArray.push(now.value);
+
+                previous.subHeadings.push(now.value);
+
+                parentPointer.set(tagName, previous);
+            } else if (previous.tagName >= tagName) {
+                if (parentPointer.get(tagName)) {
+                    parentPointer.get(tagName).subHeadings.push(now.value);
+                }
+            } else {
+                return {};
+            }
+
+            previous = now.value;
+        }
+
+        return result;
+    };
+
+    const createList = now => {
+        const li = document.createElement("LI");
+        const anchor = document.createElement("A");
+
+        anchor.textContent = now.textContent;
+        anchor.href = `#${now.id}`;
+        li.style.listStyle = "none";
+        li.style.fontSize = ".9em";
+
+        li.appendChild(anchor);
+
+        return li;
+    }
+
+    const createNestedList = (now) => {
+        const li = createList(now);
+
+        if (now.subHeadings.length) {
+            const ul = document.createElement("UL");
+            li.appendChild(ul);
+
+            now.subHeadings.forEach(
+                child => {
+                    ul.appendChild(createNestedList(child))
+                }
+            );
+        }
+
+        return li;
+    }
+
+    const convertHeadingElementChainToListElement = (now) => {
+        const ul = document.createElement("UL");
+
+        ul.appendChild(createNestedList(now));
+
+        return ul;
+    };
+
+    const createTOCObject = pipe(
+        getHeadingElementsList,
+        normalizeHeadingElementList,
+        getHeadingElementsChain
+    );
+
+    const createTOC = pipe(
+        createTOCObject,
+        convertHeadingElementChainToListElement
+    );
+
+    const toc = document.getElementsByClassName("toc")[0];
+    const title = document.getElementsByClassName("title")[0];
+    if (toc) {
+        try {
+        const content = document.getElementsByClassName("content")[0];
+        const tocObj = createTOCObject(content);
+        
+        tocObj.textContent = title.textContent;
+        tocObj.id = "";
+
+        const result = convertHeadingElementChainToListElement(tocObj);
+        const header = document.createElement("H2");
+        header.textContent = "목차";
+
+        toc.appendChild(header);
+        toc.appendChild(result);
+        } catch(e) {
+            console.log("목차 없음");
+        }
+    }
+}
